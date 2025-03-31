48473907b725ddcf291e2656f1762e2b2e1478ff
wyr0o <brykin-2006@mail.ru>
2025-03-31 19:22:28 +0300

    Add files via upload

---
+++ вебинар 8 задание 1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void counting_function()
+{
+    static int count = 0;
+    count += 1;
+    std::cout << "Количество вызовов функции counting_function() : " << count<< std::endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
