55b6661dd07c8013661eea8e5af8cd69fee71906
wyr0o <brykin-2006@mail.ru>
2025-04-01 18:38:51 +0300

    Add files via upload

---
+++ вебинар 8 задание 2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+namespace Addition {
+    int calc(int x, int y) {
+        return x + y;
+    }
+}
+
+namespace Subtraction {
+    int calc(int x, int y) {
+        return x - y;
+    }
+}
+
+namespace Multiplication {
+    int calc(int x, int y) {
+        return x * y;
+    }
+}
+
+namespace Division {
+    int calc(int x, int y) {
+        return x / y;
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int x = 6;
+    int y = 9;
+
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+
+    std::cout << "Сложение: " << Addition::calc(x, y) << std::endl;
+    std::cout << "Вычитание: " << Subtraction::calc(x, y) << std::endl;
+    std::cout << "Умножение: " << Multiplication::calc(x, y) << std::endl;
+    std::cout << "Деление: " << Division::calc(x, y) << std::endl;
+
+    return 0;
+}
